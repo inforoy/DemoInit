6616830d45402c3719e7a639e5aa3f8f77300c46
rwcalle <rwcalle@vectoritcgroup.com>
2016-12-08 11:09:22 -0500

    create

---
+++ src/test/java/demo/test/TestDemo.java
@@ -0,0 +1,13 @@
+package demo.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestDemo {
+    
+    @Test
+    public void testDemoInit(){
+        String cadena = "Roy";
+        Assert.assertEquals(cadena, "Roy");
+    }
+}
